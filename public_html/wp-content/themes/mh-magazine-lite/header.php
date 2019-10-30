a1f504928ccd30993de11f198606333586914507
Rafael Lachica Garrido <rafaellg8@correo.ugr.es>
2019-10-30 20:33:56 +0100

    Update header.php
    Cambio descripción. Añado propiedades para meta seo

--- public_html/wp-content/themes/mh-magazine-lite/header.php
+++ public_html/wp-content/themes/mh-magazine-lite/header.php
@@ -3,9 +3,14 @@
 <head>
     <meta charset="<?php bloginfo('charset'); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="Granada Pro Cycling. Web del ciclismo amateur y profesional. Ciclismo de granada. Ciclismo granadino.">
-    <meta name"keywords" content="Granada Pro Cycling, GPRC, ciclismo, web, granada, Granada, amateur, profesional">
+    <meta name="description" content="Granada Pro Cycling GRPC. La información del #CiclismodeGranada  
+Amateur y Profesional. Noticias, entrevistas, cicloturismo...">
+    <meta name"keywords" content="Granada Pro Cycling, GRPC, ciclismo, web, granada, Granada, amateur, profesional">
     <meta author="Rafael Lachica Garrido">
+    <meta property="og:title" content="Granada Pro Cycling" />
+    <meta property="og:type" content="website" />
+    <meta property="og:url" content="https://granadaprocycling.com" />
+    <meta property="og:image" content="https://granadaprocycling.com/wp-content/banners/favicons/grpc-favicon.png" />
     <meta name="robots" content="Index,Follow"
     <link rel="profile" href="http://gmpg.org/xfn/11" />
     <?php if (is_singular() && pings_open(get_queried_object())) : ?>
@@ -17,7 +22,7 @@
     <?php wp_head(); ?>
 </head>
 <body id="mh-mobile" <?php body_class(); ?> itemscope="itemscope" itemtype="http://schema.org/WebPage">
-    <title>Granada Pro Cycling</title>
+<title>Granada Pro Cycling</title>
 <?php mh_before_header();
 get_template_part('content', 'header');
 mh_after_header(); ?>
